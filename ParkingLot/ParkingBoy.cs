969b7c731c94362cbfbf7dff3ea42172d5799534
Jinglin Wang <JWang317@slb.com>
2023-11-03 17:33:54 +0800

    feat: add ParkingBoy and ut

---
+++ ParkingLot/ParkingBoy.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotManagement
+{
+    public class ParkingBoy
+    {
+        private ParkingLot parkinglot;
+
+        public ParkingBoy()
+        {
+        }
+
+        public ParkingBoy(ParkingLot p)
+        {
+            Parkinglot = p;
+        }
+
+        public ParkingLot Parkinglot { get => parkinglot; set => parkinglot = value; }
+    }
+}
--- ParkingLotTest/ParkingLotManagementTest.cs
+++ ParkingLotTest/ParkingLotManagementTest.cs
@@ -99,5 +99,100 @@ namespace ParkingLotManagementTest
             var exception = Assert.Throws<WrongTicketExceptoion>(action);
             Assert.Equal("No available position.", exception.Message);
         }
+
+        [Theory]
+        [InlineData("Car")]
+        [InlineData("Car2")]
+        public void Should_parking_boy_return_ticket_when_costumer_parking_given_car(string carName)
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+            Ticket ticket = parkingBoy.Parkinglot.Park(carName);
+
+            string carReceived = parkingBoy.Parkinglot.Fetch(ticket);
+
+            Assert.Equal(carName, carReceived);
+        }
+
+        [Theory]
+        [InlineData("Car1", "Car2")]
+        public void Should_parking_boy_return_correct_ticket_when_many_costumers_parking_given_car(string carName1, string carName2)
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+            Ticket ticket1 = parkingBoy.Parkinglot.Park(carName1);
+            Ticket ticket2 = parkingBoy.Parkinglot.Park(carName2);
+
+            string carReceived1 = parkingBoy.Parkinglot.Fetch(ticket1);
+            string carReceived2 = parkingBoy.Parkinglot.Fetch(ticket2);
+
+            Assert.Equal($"Ticket-{carName1}", ticket1.Name);
+            Assert.Equal($"Ticket-{carName2}", ticket2.Name);
+            Assert.Equal(carName1, carReceived1);
+            Assert.Equal(carName2, carReceived2);
+        }
+
+        [Theory]
+        [InlineData("Car1", "Car2")]
+        public void Should_parking_boy_return_empty_when_costumer_using_incorrect_ticket(string carNameCorrect, string carNameFalse)
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+            Ticket ticketCorrect = parkingBoy.Parkinglot.Park(carNameCorrect);
+            Ticket ticketFalse = new Ticket(carNameFalse);
+
+            Action action = () => parkingBoy.Parkinglot.Fetch(ticketFalse);
+
+            var exception = Assert.Throws<WrongTicketExceptoion>(action);
+            Assert.Equal("Unrecognized parking ticket.", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("Car1")]
+        public void Should_parking_boy_return_empty_when_costumer_using_null_ticket(string carNameCorrect)
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+            Ticket ticketCorrect = parkingBoy.Parkinglot.Park(carNameCorrect);
+            Ticket ticketNull = null;
+
+            Action action = () => parkingBoy.Parkinglot.Fetch(ticketNull);
+
+            var exception = Assert.Throws<WrongTicketExceptoion>(action);
+            Assert.Equal("Unrecognized parking ticket.", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("Car")]
+        [InlineData("Car2")]
+        public void Should_parking_boy_throw_exception_when_costumer_fetching_car_with_used_ticket(string carName)
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+            Ticket ticket = parkingBoy.Parkinglot.Park(carName);
+            string carReceived = parkingBoy.Parkinglot.Fetch(ticket);
+
+            Action action = () => parkingBoy.Parkinglot.Fetch(ticket);
+
+            var exception = Assert.Throws<WrongTicketExceptoion>(action);
+            Assert.Equal("Unrecognized parking ticket.", exception.Message);
+        }
+
+        [Theory]
+        [InlineData("Car", 10)]
+        public void Should_parking_boy_return_no_ticket_when_parking_lot_has_no_position_left(string carName, int capacity)
+        {
+            ParkingLot parkingLot = new ParkingLot();
+            ParkingBoy parkingBoy = new ParkingBoy(parkingLot);
+            for (var i = 0; i < capacity; i++)
+            {
+                Ticket teampTicket = parkingBoy.Parkinglot.Park(carName + i);
+            }
+
+            Action action = () => parkingBoy.Parkinglot.Park(carName + capacity);
+
+            var exception = Assert.Throws<WrongTicketExceptoion>(action);
+            Assert.Equal("No available position.", exception.Message);
+        }
     }
 }
