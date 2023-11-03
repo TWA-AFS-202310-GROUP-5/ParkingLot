997b2d7b8f331a4be3daadbea4d7b442d9f42488
Yang Liu <YLiu148@slb.com>
2023-11-03 16:21:15 +0800

    feat: A customer can park and fetch his car.

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLot
+{
+    public class Parking
+    {
+        private string car;
+
+        public string Park(string car)
+        {
+            this.car = car;
+            return "ticket";
+        }
+
+        public string Fetch(string ticket)
+        {
+            return "car";
+
+        }
+    
+}
+}
---
+++ ParkingLotTest/ParkingLotTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using ParkingLot;
+
+namespace ParkingLotTest
+{
+    public class ParkingLotTest
+    {
+        [Fact]
+        public void Should_return_car_when_fetch_given_ticket()
+        {
+            var car = "car";
+
+            var parkingLot = new Parking();
+            var ticket = parkingLot.Park(car);
+            var result = parkingLot.Fetch(ticket);
+
+            Assert.Equal(car, result);
+        }
+    }
+}
