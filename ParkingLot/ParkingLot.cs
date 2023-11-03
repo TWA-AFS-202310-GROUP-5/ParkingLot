bf233bda59c1deb7d4c566fafaa2e85b35155cd6
Yang Liu <YLiu148@slb.com>
2023-11-03 16:39:40 +0800

    feat: Everyone can not get a car with a invalid ticket

--- ParkingLot/ParkingLot.cs
+++ ParkingLot/ParkingLot.cs
@@ -3,23 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
 
 namespace ParkingLot
 {
     public class Parking
     {
-        private string car;
+        private Dictionary<string, string> ticket2car = new Dictionary<string, string>();
 
         public string Park(string car)
         {
-            this.car = car;
-            return "ticket";
+            var ticket = "T-" + car;
+            this.ticket2car.Add(ticket,car);
+            return ticket;
         }
 
         public string Fetch(string ticket)
         {
-            return "car";
-
+            if (ticket2car.ContainsKey(ticket))
+            {
+                var car  = ticket2car[ticket];
+                this.ticket2car.Remove(ticket);
+                return car;
+            }
+            return "";
         }
     
 }
--- ParkingLotTest/ParkingLotTest.cs
+++ ParkingLotTest/ParkingLotTest.cs
@@ -21,5 +21,44 @@ namespace ParkingLotTest
 
             Assert.Equal(car, result);
         }
+
+        [Fact]
+        public void should_return_right_when_park_given_car()
+        {
+            var parkingLot = new Parking();
+            var ticket1 = parkingLot.Park("car1");
+            var ticket2 = parkingLot.Park("car2");
+
+            var car1 = parkingLot.Fetch(ticket1);
+            var car2 = parkingLot.Fetch(ticket2);
+
+            Assert.Equal("car1", car1);
+            Assert.Equal("car2", car2);
+        }
+
+        [Fact]
+        public void Should_return_empty_when_fetch_given_wrong_ticket()
+        {
+            var parkingLot = new Parking();
+            var ticket1 = parkingLot.Park("car1");
+            var ticket2 = parkingLot.Park("car2");
+
+            var car = parkingLot.Fetch("ticket do not exit.");
+
+            Assert.Equal("",car);
+        }
+
+        [Fact]
+        public void Should_return_empty_when_fetch_given_used_ticket()
+        {
+            var parkingLot = new Parking();
+            var ticket = parkingLot.Park("car1");
+
+            var car = parkingLot.Fetch(ticket);
+
+            var result = parkingLot.Fetch(ticket);
+
+            Assert.Equal("", result);
+        }
     }
 }
