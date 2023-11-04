9a59cfcfccd8e42a90e9838131addcdd0fed4664
Zhiping Shu <ZShu@slb.com>
2023-11-04 21:14:02 +0800

    test&feat: should return nothing with err message unrecognized parking ticket when fetch the car given a smart boy manage two parking lots and a unrecognized ticket

--- ParkingLotTest/SmartParkingBoyTest.cs
+++ ParkingLotTest/SmartParkingBoyTest.cs
@@ -44,5 +44,21 @@ namespace ParkingLotTest
             Assert.Equal(car1.ID, fetchedCar1.ID);
             Assert.Equal(car2.ID, fetchedCar2.ID);
         }
+
+        [Fact]
+        public void Should_return_nothing_with_error_message_unrecognized_parking_ticket_when_fetch_the_car_given_a_smart_parking_boy_who_manage_two_parking_lots_and_an_unrecognized_ticket()
+        {
+            // given
+            string errMsg = "Unrecognized parking ticket.";
+            ParkingLot parkingLot1 = new ParkingLot(2);
+            ParkingLot parkingLot2 = new ParkingLot(2);
+            SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot[] { parkingLot1, parkingLot2 });
+            smartParkingBoy.Park(new Car());
+            Ticket unrecognizedTicket = new Ticket();
+            //when
+            var exception = Assert.Throws<WrongTicketException>(() => smartParkingBoy.Fetch(unrecognizedTicket));
+            //then
+            Assert.Equal(errMsg, exception.Message);
+        }
     }
 }
