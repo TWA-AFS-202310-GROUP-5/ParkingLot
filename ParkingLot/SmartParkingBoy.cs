7c48bb2888f50e80e31f95f0ee823d080baa8e34
Zhiping Shu <ZShu@slb.com>
2023-11-04 20:36:32 +0800

    test: test case for car should be parked to parking lot which contains more empty positions when park the car given a smart parking boy manage two parking lots both with available postion and a car

---
+++ ParkingLot/SmartParkingBoy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotManage
+{
+    public class SmartParkingBoy : StandardParklotBoy
+    {
+        public SmartParkingBoy(ParkingLot parkingLot) : base(parkingLot)
+        {
+        }
+
+        public SmartParkingBoy(ParkingLot[] parkingLots) : base(parkingLots)
+        {
+        }
+    }
+}
---
+++ ParkingLotTest/SmartParkingBoyTest.cs
@@ -0,0 +1,29 @@
+﻿using ParkingLotManage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class SmartParkingBoyTest
+    {
+        [Fact]
+        public void Should_car_be_parked_to_parking_lot_contains_more_positions_when_park_the_car_given_a_standard_parking_boy_manage_two_parking_lots_both_with_available_position_and_a_car()
+        {
+            //given
+            ParkingLot parkingLot1 = new ParkingLot(2);
+            ParkingLot parkingLot2 = new ParkingLot(2);
+            SmartParkingBoy smartParkingBoy = new SmartParkingBoy(new ParkingLot[] {parkingLot1, parkingLot2});
+            Car car1 = new Car();
+            Car car2 = new Car();
+            smartParkingBoy.Park(car1);
+            //when
+            Ticket ticket = smartParkingBoy.Park(car2);
+            //then
+            Assert.Equal(parkingLot2.ParkingLotId, ticket.ParkingLotId);
+        }
+    }
+}
