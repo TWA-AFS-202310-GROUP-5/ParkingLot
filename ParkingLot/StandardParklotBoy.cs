6b0f7b471058a231529471ce217784fb492e324c
Zhiping Shu <ZShu@slb.com>
2023-11-04 15:16:28 +0800

    test: test case for return a parking ticket when park the car given a standard parking boy, parking lot and a car

---
+++ ParkingLot/StandardParklotBoy.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParkingLotManage
+{
+    public class StandardParklotBoy
+    {
+        private ParkingLot parkingLot;
+        public StandardParklotBoy(ParkingLot parkingLot)
+        {
+            this.parkingLot = parkingLot;
+        }
+
+        public string Park(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ParkingLotTest/StandardParkingLotBoyTest.cs
@@ -0,0 +1,26 @@
+﻿using ParkingLotManage;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ParkingLotTest
+{
+    public class StandardParkingLotBoyTest
+    {
+        [Fact]
+        public void Should_return_a_parking_ticket_when_park_the_car_given_a_parking_lot_a_standard_parking_boy_and_a_car()
+        {
+            // given
+            ParkingLot parkingLot = new ParkingLot();
+            StandardParklotBoy standardParkinglotBoy = new StandardParklotBoy(parkingLot);
+            //when
+            string ticket = standardParkinglotBoy.Park("car");
+            //then
+            Assert.Equal("T-car", ticket);
+        }
+
+    }
+}
